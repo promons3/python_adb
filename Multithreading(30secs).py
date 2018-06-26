9ea58912d567b7c4f1d3d7cad06a2c602f1eaf02
birendra <promons3@gmail.com>
2018-06-26 18:43:47 +0530

    modified the logging style to get complete logs

--- Multithreading(30secs).py
+++ Multithreading(30secs).py
@@ -11,12 +11,14 @@ import shutil
 import codecs
 import glob
 import re
+import signal
 
 from subprocess import STDOUT, check_output
 #from multiprocessing.dummy import Pool as ThreadPool
 from multiprocessing import Pool
 from functools import partial
 from subprocess import DEVNULL
+from subprocess import Popen
 
 
 def split_lines(s):
@@ -51,19 +53,19 @@ class Multi():
      
     def folder_creation(filepath):
         #global folderpath
-        apklist = []
+        apkfiles_list = []
         folderpath = input('Please provide the path of APKs folder:-')
         
         if os.path.exists(folderpath):
             directory = os.path.normpath(folderpath)
-            apklist = glob.glob(os.path.join(folderpath, '*.apk'))
-            if not apklist:
+            apkfiles_list = glob.glob(os.path.join(folderpath, '*.apk'))
+            if not apkfiles_list:
                 print('')
                 print('******* in the given path, .apk files are not found. Please check once again. ********')
                 time.sleep(1)
                 exit()
             else:    
-                return apklist
+                return apkfiles_list
                         
         else:
             print('')
@@ -74,7 +76,7 @@ class Multi():
     
     def collect_deviceid(self):
         
-        line2 = []
+        device_id = []
         out = split_lines(subprocess.check_output(['adb', 'devices']).decode('utf-8'))
         for line in out[1:]:
             
@@ -84,22 +86,22 @@ class Multi():
                 continue
             if 'unauthorized' in line: 
                 continue
-            if 'unknwon' in line: 
+            if 'unknown' in line: 
                 continue    
 
             serial, _ = re.split(r'\s+', line, maxsplit=1)
-            line2.append(serial)     
-        return line2
+            device_id.append(serial)     
+        return device_id
     
-    def app_installation(self, apk2, packagename, activityname, line1):
+    def app_installation(self, apkfile_path, packagename, activityname, line1):
         
-        self.apk2 = apk2
+        self.apkfile_path = apkfile_path
         self.line1 = line1     
-        line2 = ''.join([str(x) for x in line1]) 
-        apk3 = ''.join([str(x1) for x1 in apk2])
-        dir_path = os.path.dirname(os.path.realpath(apk2))
+        device_id = ''.join([str(x) for x in line1]) 
+        actual_filepath = ''.join([str(x1) for x1 in apkfile_path])
+        dir_path = os.path.dirname(os.path.realpath(apkfile_path))
         os.chdir(dir_path)
-        file_name = os.path.basename(apk2).rsplit('.', 1)[0]
+        file_name = os.path.basename(apkfile_path).rsplit('.', 1)[0]
         #print('directory path with file name', dir_path) juzt to check directory path
         today = datetime.date.today()
         subdir = "AppTesting_On_"
@@ -119,58 +121,72 @@ class Multi():
         DEVNULL = open(os.devnull, 'wb')
         
         try:
-            device_name = subprocess.check_output(['adb', '-s', line2, 'shell', 'getprop', 'ro.product.model']).decode('ascii').strip()
+            device_name = subprocess.check_output(['adb', '-s', device_id, 'shell', 'getprop', 'ro.product.model']).decode('ascii').strip()
         except subprocess.CalledProcessError as e:
             print(e)   
         
-        pit = subprocess.Popen(['adb', '-s', line2, 'install', '-r', apk3], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+        pit = subprocess.Popen(['adb', '-s', device_id, 'install', '-r', actual_filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
         pipe, err = pit.communicate()
-        file = open('%s_hi.txt' %line2, 'wb')
+        file = open('%s_hi.txt' %device_id, 'wb')
         pickle.dump((pipe, err), file) # to add pipe and err objects in a one text file, so used pickle
         file.close()
-        with open('%s_hi.txt' %line2, 'rb') as f2:
+        with open('%s_hi.txt' %device_id, 'rb') as f2:
             l1 = f2.readlines()
             for l2 in l1:
-                if b"Failure" in l2:
-                    l2 = l2.rstrip()
-                    l2 = l2.decode('utf8', 'ignore')
-                    print('your application is NOT INSTALLED on', device_name, 'as an error message appears', l2)
-                    print('')
-                    break       
-            else:
-                process2 = subprocess.Popen(['adb', '-s', line2, 'logcat', '-c'], stdout=DEVNULL, shell=True)
-                launch = subprocess.Popen(['adb', '-s', line2, 'shell', 'am', 'start', '-n', packagename +'/' + activityname], stdout=DEVNULL, shell=True)
-                print("App has launched on device", device_name, "Please check out on the respective device.")
-                print('')
-                time.sleep(30)
-                with open('%s.txt' %line2, 'w+') as f1:
-                    subprocess.Popen(['adb', '-s', line2, 'logcat', '-v', 'threadtime', '-d'], stdout=f1)
-                    f1.flush()
+                if b"Failure [INSTALL_FAILED_TEST_ONLY" in l2:
+                    pit = subprocess.Popen(['adb', '-s', device_id, 'install', '-t', actual_filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
                     time.sleep(1)
-                f1.close()
-                
-                try:    
-                    process2 = subprocess.Popen(['adb', '-s', line2, 'shell', 'am', 'force-stop', activityname], stdout=DEVNULL, shell=True)
-                except(OSError)as e:
-                    if e.errno != os.errno.EXIST:
-                        print(e.errno)
-                        raise
-                try:   
-                    process3 = subprocess.Popen(['adb', '-s', line2, 'shell', 'pm', 'clear', packagename], stdout=DEVNULL, stderr=subprocess.PIPE, shell=True)
                     
-                except(OSError, IOError) as e:
-                    print(e)
-                try: 
-                    print('')                
-                    process4 = subprocess.Popen(['adb', '-s', line2, 'shell', 'pm', 'uninstall', packagename], stdout=DEVNULL, shell=True)
-                    print("Your app has uninstalled from the device", device_name)
-                    print('')  
-                except(OSError, IOError) as e:
-                    print(e)                        
+                elif b"Failure" in l2:
+                        l2 = l2.rstrip()   
+                        l2 = l2.decode('utf8', 'ignore')
+                        print('Your application is NOT INSTALLED on', device_name, 'as an error message appears', l2)
+                        print('')
+                        break
+                
+            else:
+                process2 = subprocess.Popen(['adb', '-s', device_id, 'logcat', '-c'], stdout=DEVNULL)
+                
+                with open('%s.txt' %device_id, 'w') as f1:
+                    #print(device_id)                
+                    process5 = subprocess.Popen(['adb', '-s', device_id, 'logcat', '-v', 'threadtime'], stdout=f1, shell=True)
+                    f1.flush()
+                    #print('PID is ' + str(process5.pid))
+                    launch = subprocess.Popen(['adb', '-s', device_id, 'shell', 'am', 'start', '-n', packagename +'/' + activityname], stdout=DEVNULL, shell=True)
+                    print("App has launched on device", device_name, "Please check out on the respective device.")
                     print('')
-                                                                 
+                    time.sleep(30)
+                
+                    try:  
+                        time.sleep(1)
+                        process2 = subprocess.Popen(['adb', '-s', device_id, 'shell', 'am', 'force-stop', activityname], stdout=DEVNULL, shell=True)                       
+                    except(OSError)as e:
+                        if e.errno != os.errno.EXIST:
+                            print(e.errno)
+                            raise
+                    try:   
+                        process3 = subprocess.Popen(['adb', '-s', device_id, 'shell', 'pm', 'clear', packagename], stdout=DEVNULL, stderr=subprocess.PIPE, shell=True)
+                    except(OSError, IOError) as e:
+                        print(e)
+                    try: 
+                        print('')                
+                        process4 = subprocess.Popen(['adb', '-s', device_id, 'shell', 'pm', 'uninstall', packagename], stdout=DEVNULL)
+                        print("Your app has uninstalled from the device", device_name)
+                        print('')  
+                    except(OSError, IOError) as e:
+                        print(e)                        
+                        print('')
+                    #print('PID is at bottom is ' + str(process5.pid))
+                     
+                    Popen("TASKKILL /F /PID {pid} /T".format(pid=process5.pid), stdout=DEVNULL)                    
+                    #process6 = subprocess.Popen(['adb', '-s', device_id, 'logcat', '-c'], stdout=DEVNULL)
+                    f1.flush()    
+                    #time.sleep(.30)           
+                f1.close() 
+                process6 = subprocess.Popen(['adb', '-s', device_id, 'logcat', '-c'], stdout=DEVNULL)
+                
         f2.close()
-        os.remove('%s_hi.txt' %line2)
+        os.remove('%s_hi.txt' %device_id)
         
 
 
@@ -182,14 +198,14 @@ if __name__ == "__main__":
         print('*******Devices are not found in \'device\' mode, or devices are not connected. please check the connection(s).*******')
         time.sleep(1)
         exit()
-    apk4 = c.folder_creation()
+    allapk_files = c.folder_creation()
     print('')
     print("***********Build installation is going on...**************...Please be patient & keep smiling :)")
     print('')
     p = Pool()
-    for apk2 in apk4:
-        print('Build Name is:-', apk2)
-        pipe = subprocess.Popen(['aapt', 'dump', 'badging', apk2], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, encoding = "utf-8", errors='ignore')
+    for apkfile_path in allapk_files:
+        print("Build's full path is:-", apkfile_path)
+        pipe = subprocess.Popen(['aapt', 'dump', 'badging', apkfile_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, encoding = "utf-8", errors='ignore')
         result = pipe.communicate()[0] #byte to string coversion used decode method.
         result =result.strip()
         
@@ -220,7 +236,7 @@ if __name__ == "__main__":
             
         #l = multiprocessing.Lock()
         try:
-            func = partial(c.app_installation, apk2, packagename, activityname)
+            func = partial(c.app_installation, apkfile_path, packagename, activityname)
             p.map(func, line1)
         except KeyboardInterrupt:
             p.close()
@@ -229,7 +245,7 @@ if __name__ == "__main__":
     #p.close()
     p.terminate()
     p.join()
-    print('*******done with testing of all availble apps in the given path with all connected devices.*******')
+    print('*******Done with testing of all availble apps in the given path with all connected devices.*******')
     #os.remove('devicename.txt')
     exit()
    
